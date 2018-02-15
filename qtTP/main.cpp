d3f73fc5837b58eb938750ebb23dbdd02041d83d
Remy Bouteloup <remy.bouteloup@etu.univ-tours.fr>
2018-02-15 14:38:16 +0100

    Exercice 1 OK !

--- qtTP/main.cpp
+++ qtTP/main.cpp
@@ -1,11 +1,18 @@
 #include "mainwindow.h"
+#include "dialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    Dialog d;
+    int dialogCode = d.exec();
+
+    if (dialogCode == QDialog::Accepted)
+        w.show();
+    else
+        return 0;
 
     return a.exec();
 }
